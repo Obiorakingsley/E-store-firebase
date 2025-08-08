b6ecec6be599695aeee742295c311c5ae6c56d44
Obiora Kingsley <kingsleyobiora527@gmail.com>
2025-08-08 15:15:20 +0100

    Added flash sales section with countdown timer ans a pulse animation to CTA button in Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,55 @@
 import React from "react";
 import "./Styles/Hero.css";
-import { FaArrowCircleRight } from "react-icons/fa";
+import { FaArrowCircleRight, FaChevronRight, FaTag } from "react-icons/fa";
+import { GiPriceTag } from "react-icons/gi";
+import { useState, useEffect } from "react";
 
 function Hero() {
+  // State to manage the countdown timer
+  const [timeLeft, setTimeLeft] = useState({
+    hours: 21,
+    minutes: 18,
+    seconds: 20,
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => {
+        if (prev.seconds > 0) return { ...prev, seconds: prev.seconds - 1 };
+        if (prev.minutes > 0)
+          return { ...prev, minutes: prev.minutes - 1, seconds: 59 };
+        if (prev.hours > 0)
+          return { hours: prev.hours - 1, minutes: 59, seconds: 59 };
+        if (prev.hours < 1)
+          return { hours: prev.hours + 12, minutes: 59, seconds: 59 };
+        clearInterval(timer);
+        return { hours: 0, minutes: 0, seconds: 0 };
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <section className="hero-section">
+      <div className="flash-sales">
+        <div className="tag-section">
+          <GiPriceTag size={40} />
+          <h2>Flash Sales</h2>
+        </div>
+
+        <div className="countdown-section">
+          <h2>
+            Time Left: {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s
+          </h2>
+        </div>
+
+        <div className="nav-section">
+          <p>
+            See all <FaChevronRight />
+          </p>
+        </div>
+      </div>
       <div className="cta-container">
         <h1>Deals you can't resist</h1>
         <h2>
